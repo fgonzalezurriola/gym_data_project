7035c0df830e14a436a75bbd44afc5b6100d3e27
Franzkier <140219655+fgonzalezurriola@users.noreply.github.com>
2024-11-10 11:26:28 -0300

    Fix: changed gunicorn config

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -1,6 +1,5 @@
 import os
 
-port = int(os.getenv("PORT", 8000))
-bind = f"0.0.0.0:{port}"
+bind = f"0.0.0.0:{os.getenv('PORT', '8000')}"
 workers = 4
 worker_class = "uvicorn.workers.UvicornWorker"
